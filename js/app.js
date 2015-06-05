aaef4d0fbd359956dda5cd62b16441f97951905c
alik864 <alikarukas@gmail.com>
2015-06-05 11:31:40 -0400

    Creating a Post

--- js/app.js
+++ js/app.js
@@ -1,10 +1,51 @@
 $(document).ready(function(){
 
-  $.ajax({
+  // Add a click handler to the submit button
+  // for adding a new post
+  $('#create-post').on('click',function(){
+
+    // form the data that you will send to the server
+    // to create a new post in the backend.
+    var postData = {
+      post: {
+        title: $('#title').val(),
+        content: $('#content').val()
+      }
+    };
+
+    // sent the post data the user entering
+    // to the server
+    $.ajax({
+      url: 'http://localhost:3000/posts',
+      method: 'POST',
+      data: postData,
+      dataType: 'json'
+    })
+    .done(function(result){
+      console.log("Added post");
+      $('#title').val('');
+      $('#content').val('');
+      getAllPosts();
+    })
+    .fail(function(){
+      alert('Error: failed to create a new post');
+    })
+
+  });
+
+  getAllPosts();
+
+});
+
+
+function getAllPosts(){
+   $.ajax({
     url: 'http://localhost:3000/posts',
     dataType: 'json'
   })
   .done(function(posts_data){
+    $('#all-posts').html('');
+
     // posts_data is an array of all the posts
     console.log(posts_data);
 
@@ -15,4 +56,4 @@ $(document).ready(function(){
     })
   });
 
-})
+}
