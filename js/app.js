238a356f399e1067dfc635007482ef8bf935564c
alik864 <alikarukas@gmail.com>
2015-06-05 18:44:38 -0400

    updated the category option and code works

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
 $(document).ready(function(){
 
+
+   $('#category-filter').on('change',function(){
+var category_id = $("#category-filter option:selected").val();
+  getPostsInCategory(category_id);
+  });
+
+
   // Add a click handler to the submit button
   // for adding a new post
-  $('#create-post').on('click',function(){
 
+  $('#create-post').on('click',function(){
+var category_id = $("#categories option:selected").val();
     // form the data that you will send to the server
     // to create a new post in the backend.
     var postData = {
       post: {
         title: $('#title').val(),
-        content: $('#content').val()
+        content: $('#content').val(),
+        category_id: category_id
       }
     };
 
@@ -33,11 +42,13 @@ $(document).ready(function(){
     // if there is error then an alert will come up
     .fail(function(){
       alert('Error: failed to create a new post');
-    })
+    });
 
   });
 
   getAllPosts();
+
+  getAllCategories();
 
 });
 
@@ -57,8 +68,51 @@ function getAllPosts(){
     // for each individual post in the posts_data array
     posts_data.forEach(function(post){
       // create a HTML paragraph with one post's title in it.
-      $('#all-posts').append("<li class='post-entry' id='" + post.id + "'>" + post.title + "<p>" + post.content + "</p></li>");
-    })
+      $('#all-posts').append("<li class='post-entry' id='" + post.id + "'>" + post.title + "<p>" + post.date + "</p>" + "<p>" + post.content + "</p></li>");
+    });
   });
 
+}
+
+function getPostsInCategory(category_id){
+   $.ajax({
+    url: 'http://localhost:3000/posts?q=' + category_id,
+    dataType: 'json'
+  })
+  .done(function(posts_data){
+    $('#all-posts').html('');
+
+    // posts_data is an array of all the posts
+    console.log(posts_data);
+
+    // for each individual post in the posts_data array
+    posts_data.forEach(function(post){
+      // create a HTML paragraph with one post's title in it.
+      $('#all-posts').append("<li class='post-entry' id='" + post.id + "'>" + post.title + "<p>" + post.date + "</p>" + "<p>" + post.content + "</p></li>");
+    });
+  });
+
+}
+
+function getAllCategories(){
+  $.ajax({
+    url: 'http://localhost:3000/categories',
+    dataType: 'json'
+  })
+  .done(function(data){
+    $('#categories').html('');
+
+ $('#category-filter').html('');
+
+
+    // posts_data is an array of all the posts
+    console.log(data);
+
+    // for each individual post in the posts_data array
+    data.forEach(function(element){
+      // create a HTML paragraph with one post's title in it.
+      $('#categories').append("<option value='" + element.id + "'>" + element.name + "</option>");
+       $('#category-filter').append("<option value='" + element.id + "'>" + element.name + "</option>");
+    });
+  });
 }
