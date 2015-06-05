7002bdfa39a6f695863ea9e6a3b9dd074a68ed3a
alik864 <alikarukas@gmail.com>
2015-06-04 21:04:00 -0400

    javascript to show each post

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
-  alert("My app is here");
+
+  $.ajax({
+    url: 'http://localhost:3000/posts',
+    dataType: 'json'
+  })
+  .done(function(posts_data){
+    // posts_data is an array of all the posts
+    console.log(posts_data);
+
+    // for each individual post in the posts_data array
+    posts_data.forEach(function(post){
+      // create a HTML paragraph with one post's title in it.
+      $('#all-posts').append("<li class='post-entry' id='" + post.id + "'>" + post.title + "<p>" + post.content + "</p></li>");
+    })
+  });
 
 })
